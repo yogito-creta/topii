e816af7ca6db6b9ed337b8362d20b57445ef5374
yogito-creta <nuriyeaksakal19@gmail.com>
2026-01-28 17:31:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
+// files klasörü
 app.use("/files", express.static(path.join(__dirname, "files")));
 
+// ANA SAYFA
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("topii çalışıyor:", PORT);
+  console.log("Server çalışıyor:", PORT);
 });
